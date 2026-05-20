af5a035bdee86631e882c239eae3dafbf9f9122f
Fairfax18 <edward.chndr97@gmail.com>
2026-05-20 11:10:56 +0700

    add vercel's speedinsight

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import './globals.css';
 import AppLayout from '../components/AppLayout';
 
@@ -9,8 +10,9 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className="bg-white dark:bg-[#0a0a0a] text-neutral-900 dark:text-neutral-100 transition-colors duration-300">
         <AppLayout>{children}</AppLayout>
-        
+
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
